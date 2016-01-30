0ebd51a7e7bec6251ca82f74fe08b5490b6e5f36
Neraste <neraste.herr10@gmail.com>
2016-01-30 15:19:06 +0100

    Simplify local settings management

--- settings.py
+++ settings.py
@@ -4,16 +4,8 @@ CREDENTIALS = ()
 LOGGING_LEVEL = "INFO"
 DELAY_BETWEEN_REQUESTS = 1
 REQUESTS_LOGGING_DISABLED = True
-try:
-    import local_settings
 
-    if hasattr(local_settings, 'KARA_FOLDER_PATH'):
-        KARA_FOLDER_PATH = local_settings.KARA_FOLDER_PATH
-    if hasattr(local_settings, 'SERVER_URL'):
-        SERVER_URL = local_settings.SERVER_URL
-    if hasattr(local_settings, 'CREDENTIALS'):
-        CREDENTIALS = local_settings.CREDENTIALS
-    if hasattr(local_settings, 'LOGGING_LEVEL'):
-        LOGGING_LEVEL = local_settings.LOGGING_LEVEL
+try:
+    from local_settings import *
 except ImportError:
     pass
