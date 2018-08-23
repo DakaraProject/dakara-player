621369074a6b943efce3ea61120ff174789394f8
Neraste <neraste.herr10@gmail.com>
2018-08-23 14:45:12 +0200

    Adapt bumb version script

--- bump_version.sh
+++ bump_version.sh
@@ -11,16 +11,22 @@ fi
 version_number=$1
 version_date=$(date -I -u)
 
+# change internal version file
 version_file=dakara_player_vlc/version.py
 cat <<EOF >$version_file
 __version__ = '$version_number'
 __date__ = '$version_date'
 EOF
 
+# change version in changelog
 changelog_file=CHANGELOG.md
 sed -i "/^## Unreleased$/a \\
 \\
 ## $version_number - $version_date" $changelog_file
+
+# change version in appveyor config file
+appveyor_file=appveyor.yml
+sed -i "s/^version: .*-{build}$/version: $version_number-{build}/" $appveyor_file
 
 git add $version_file $changelog_file
 git commit -m "Version $version_number" --no-verify
