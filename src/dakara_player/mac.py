9bf3641e1371d05dad49aacd89a732f554c5282a
Neraste <neraste.herr10@gmail.com>
2022-05-05 20:31:35 +0900

    Add mac library to laod Tk

---
+++ src/dakara_player/mac.py
@@ -0,0 +1,125 @@
+import os
+import sys
+import tkinter
+from ctypes import c_void_p, cdll
+from subprocess import CalledProcessError, check_call, run
+
+from path import Path
+
+TK_LIBRARY_PATH = "TK_LIBRARY_PATH"
+BREW = "brew"
+BREW_PREFIX = "--prefix"
+LIB = "lib"
+
+
+def check_brew():
+    """Check Brew exists.
+
+    Returns:
+        bool: `True` if Brew is available, `False` otherwise.
+    """
+    try:
+        check_call([BREW])
+        return True
+
+    except CalledProcessError:
+        return False
+
+
+def get_brew_prefix(formula):
+    """Get the prefix of a Brew formula.
+
+    Args:
+        formula (str): Brew formula.
+
+    Returns:
+        path.Path: Path of the formula prefix. `None` if the prefix cannot be
+        obtained.
+    """
+    try:
+        outcome = run(
+            [BREW, BREW_PREFIX, str(formula)],
+            capture_output=True,
+            check=True,
+            text=True,
+        )
+
+    except CalledProcessError:
+        return None
+
+    return Path(outcome.stdout.strip())
+
+
+def get_tcl_tk_lib_path():
+    """Retrieve the location of Tcl Tk library.
+
+    The location can be obtained either from the environment variable
+    `TK_LIBRARY_PATH`, or from Brew.
+
+    Returns:
+        path.Path: Path of Tck Tk library. `None` if it can't be found.
+    """
+    # if the TK_LIBRARY_PATH environment variable exists, use it
+    if TK_LIBRARY_PATH in os.environ:
+        return Path(os.environ[TK_LIBRARY_PATH])
+
+    # if Brew is not installed, don't do anything
+    if not check_brew():
+        return None
+
+    # try to obtain the location of tk
+    prefix = get_brew_prefix("tcl-tk")
+    if prefix is None:
+        return None
+
+    return prefix / LIB
+
+
+def load_get_ns_view():
+    """Load the function to get NSView.
+
+    Load Tk library for Mac.
+
+    See:
+        https://github.com/oaubert/python-vlc/blob/38a90baf1d6c1e9a6131433ec3819766f308612c/examples/tkvlc.py#L52
+
+    Returns:
+        function: Function to get NSView object.
+    """
+
+    # libtk = cdll.LoadLibrary(ctypes.util.find_library('tk'))
+    # returns the tk library /usr/lib/libtk.dylib from macOS,
+    # but we need the tkX.Y library bundled with Python 3+,
+    # to match the version number of tkinter, _tkinter, etc.
+    try:
+        libtk = "libtk{}.dylib".format(tkinter.TkVersion)
+
+        # if Python was installed with Brew, it doesn't have Tk, so Tcl-Tk must be
+        # installed separately and we have to retrieve its library path
+        lib_path = get_tcl_tk_lib_path()
+
+        if lib_path is None:
+            # use default locations
+            prefix = Path(getattr(sys, "base_prefix", sys.prefix))
+            lib_path = prefix / LIB
+
+        dylib = cdll.LoadLibrary(str(lib_path / libtk))
+
+        # getNSView = dylib.TkMacOSXDrawableView is the
+        # proper function to call, but that is non-public
+        # (in Tk source file macosx/TkMacOSXSubwindows.c)
+        # and dylib.TkMacOSXGetRootControl happens to call
+        # dylib.TkMacOSXDrawableView and return the NSView
+        _GetNSView = dylib.TkMacOSXGetRootControl
+        # C signature: void *_GetNSView(void *drawable) to get
+        # the Cocoa/Obj-C NSWindow.contentView attribute, the
+        # drawable NSView object of the (drawable) NSWindow
+        _GetNSView.restype = c_void_p
+        _GetNSView.argtypes = (c_void_p,)
+
+    except (NameError, OSError):  # image or symbol not found
+
+        def _GetNSView(unused):
+            return None
+
+    return _GetNSView
---
+++ tests/unit/test_mac.py
@@ -0,0 +1,138 @@
+import os
+from subprocess import CalledProcessError, CompletedProcess
+from unittest import TestCase, skipUnless
+from unittest.mock import patch
+
+from path import Path
+
+from dakara_player.mac import (
+    check_brew,
+    get_brew_prefix,
+    get_tcl_tk_lib_path,
+    load_get_ns_view,
+)
+
+POSIX = os.name == "posix"
+
+
+@patch("dakara_player.mac.check_call")
+class CheckBrewTestCase(TestCase):
+    def test_check(self, mocked_check_call):
+        """Test a positive call."""
+        self.assertTrue(check_brew())
+
+        mocked_check_call.assert_called_with(["brew"])
+
+    def test_check_fail(self, mocked_check_call):
+        """Test a negative call."""
+        mocked_check_call.side_effect = CalledProcessError(returncode=255, cmd="brew")
+        self.assertFalse(check_brew())
+
+
+@skipUnless(POSIX, "Tested on POSIX system only")
+@patch("dakara_player.mac.run")
+class GetBrewPrefixTestCase(TestCase):
+    def test_get(self, mocked_run):
+        """Test to get a prefix."""
+        mocked_run.return_value = CompletedProcess(
+            args="brew", returncode=0, stdout="/path/to/formula\n"
+        )
+        self.assertEqual(
+            get_brew_prefix("formula"), Path("/") / "path" / "to" / "formula"
+        )
+
+        mocked_run.assert_called_with(
+            ["brew", "--prefix", "formula"], capture_output=True, check=True, text=True
+        )
+
+    def test_get_fail(self, mocked_run):
+        """Test to get the prefix of a formula that does not exist."""
+        mocked_run.side_effect = CalledProcessError(
+            returncode=255, cmd="brew --prefix formula"
+        )
+        self.assertIsNone(get_brew_prefix("formula"))
+
+
+@skipUnless(POSIX, "Tested on POSIX system only")
+@patch("dakara_player.mac.get_brew_prefix")
+@patch("dakara_player.mac.check_brew")
+class GetTclTkLibPathTestCase(TestCase):
+    def test_get_environ(self, mocked_check_brew, mocked_get_brew_prefix):
+        """Test to get lib from environment variable."""
+        with patch.dict(
+            "dakara_player.mac.os.environ",
+            {"TK_LIBRARY_PATH": "/path/to/formula/lib"},
+            clear=True,
+        ):
+            self.assertEqual(
+                get_tcl_tk_lib_path(), Path("/") / "path" / "to" / "formula" / "lib"
+            )
+
+        mocked_check_brew.assert_not_called()
+        mocked_get_brew_prefix.assert_not_called()
+
+    def test_get_brew(self, mocked_check_brew, mocked_get_brew_prefix):
+        """Test to get lib from Brew."""
+        mocked_check_brew.return_value = True
+        mocked_get_brew_prefix.return_value = Path("/") / "path" / "to" / "formula"
+
+        with patch.dict(
+            "dakara_player.mac.os.environ",
+            {},
+            clear=True,
+        ):
+            self.assertEqual(
+                get_tcl_tk_lib_path(), Path("/") / "path" / "to" / "formula" / "lib"
+            )
+
+    def test_get_no_brew(self, mocked_check_brew, mocked_get_brew_prefix):
+        """Test when Brew is not available."""
+        mocked_check_brew.return_value = False
+
+        with patch.dict(
+            "dakara_player.mac.os.environ",
+            {},
+            clear=True,
+        ):
+            self.assertIsNone(get_tcl_tk_lib_path())
+
+        mocked_get_brew_prefix.assert_not_called()
+
+    def test_get_brew_no_prefix(self, mocked_check_brew, mocked_get_brew_prefix):
+        """Test when Brew formula does not exist."""
+        mocked_check_brew.return_value = True
+        mocked_get_brew_prefix.return_value = None
+
+        with patch.dict(
+            "dakara_player.mac.os.environ",
+            {},
+            clear=True,
+        ):
+            self.assertIsNone(get_tcl_tk_lib_path())
+
+
+@skipUnless(POSIX, "Tested on POSIX system only")
+@patch("dakara_player.mac.sys.base_prefix", "/usr")
+@patch("dakara_player.mac.tkinter.TkVersion", "0.0")
+@patch("dakara_player.mac.cdll")
+@patch("dakara_player.mac.get_tcl_tk_lib_path")
+class LoadGetNsViewTestCase(TestCase):
+    def test_load_standard(self, mocked_get_tcl_tk_lib_path, mocked_cdll):
+        """Test load from standard location."""
+        mocked_get_tcl_tk_lib_path.return_value = None
+        self.assertIsNotNone(load_get_ns_view())
+        mocked_cdll.LoadLibrary.assert_called_with("/usr/lib/libtk0.0.dylib")
+
+    def test_load_custom(self, mocked_get_tcl_tk_lib_path, mocked_cdll):
+        """Test load from custom location."""
+        mocked_get_tcl_tk_lib_path.return_value = Path("/") / "path" / "to" / "lib"
+        self.assertIsNotNone(load_get_ns_view())
+        mocked_cdll.LoadLibrary.assert_called_with("/path/to/lib/libtk0.0.dylib")
+
+    def test_load_fail(self, mocked_get_tcl_tk_lib_path, mocked_cdll):
+        """Test load from custom location."""
+        mocked_get_tcl_tk_lib_path.return_value = None
+        mocked_cdll.LoadLibrary.side_effect = OSError()
+        function = load_get_ns_view()
+        self.assertIsNotNone(function)
+        self.assertIsNone(function(None))
