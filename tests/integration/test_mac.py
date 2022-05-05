a884464b5ae0257821d0483830d27d3a1fa1beec
Neraste <neraste.herr10@gmail.com>
2022-05-05 21:43:41 +0900

    Fix Mac integration test

--- tests/integration/test_mac.py
+++ tests/integration/test_mac.py
@@ -4,7 +4,7 @@ from unittest import TestCase, skipUnless
 from dakara_player.mac import check_brew, get_brew_prefix, load_get_ns_view
 
 
-@skipUnless(platform.system == "Darwin", "Tested on Mac only")
+@skipUnless(platform.system() == "Darwin", "Tested on Mac only")
 class LoadGetNsViewIntegrationTestCase(TestCase):
     def test_load(self):
         """Test to load Tk and retrieve the function to get NSView."""
